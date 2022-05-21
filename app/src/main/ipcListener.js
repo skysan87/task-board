e5c8108e247e31df7e089c5f1e1977084c33e705
skysan <26142873+skysan87@users.noreply.github.com>
2022-05-21 11:28:31 +0900

    最前面に固定処理を削除

--- app/src/main/ipcListener.js
+++ app/src/main/ipcListener.js
@@ -3,9 +3,8 @@ const { ipcMain, BrowserWindow } = require('electron')
 exports.registerIpcListener = (window) => {
   // リサイズ
   ipcMain.on('ipc-resize-window', (ev, option) => {
-    // const { width, height, alwaysOnTop } = option
-    // window.setSize(width, height, true)
-    // window.setAlwaysOnTop(alwaysOnTop || false)
+    const { width, height } = option
+    window.setSize(width, height, true)
   }),
 
   ipcMain.on('ipc-minimize-window', () => {
