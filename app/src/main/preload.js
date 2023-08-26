752e0cf2aee4571fb975bdbaddec7f66cbb30507
skysan <26142873+skysan87@users.noreply.github.com>
2023-08-26 11:37:05 +0900

    最前面に固定の機能を廃止

--- app/src/main/preload.js
+++ app/src/main/preload.js
@@ -4,11 +4,6 @@ const { contextBridge, ipcRenderer } = require('electron')
 // ex) window.electron.メソッド名(引数)
 contextBridge.exposeInMainWorld(
   'electron', {
-    resizeWindow: (args) => ipcRenderer.send('ipc-resize-window', args),
-    minimizeWindow: () => ipcRenderer.send('ipc-minimize-window'),
-    maximizeWindow: () => ipcRenderer.send('ipc-maximize-window'),
-    normalizeWindow: () => ipcRenderer.send('ipc-normalize-window'),
-    closeWindow: () => ipcRenderer.send('ipc-close-window'),
-    stickyWindow: () => ipcRenderer.send('ipc-sticky-window')
+    resizeWindow: (args) => ipcRenderer.send('ipc-resize-window', args)
   }
 )
